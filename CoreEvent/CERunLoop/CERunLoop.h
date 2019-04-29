e94adda610be0b96f15e828c2f88b735234da5a6
vector <dev_vector@qq.com>
2019-04-29 21:13:01 +0800

    123

---
+++ CoreEvent/CERunLoop/CERunLoop.h
@@ -0,0 +1,19 @@
+//
+//  CERunLoop.h
+//  CERunLoop
+//
+//  Created by vector on 2019/4/29.
+//  Copyright © 2019 com.wangqinghai. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CERunLoop.
+FOUNDATION_EXPORT double CERunLoopVersionNumber;
+
+//! Project version string for CERunLoop.
+FOUNDATION_EXPORT const unsigned char CERunLoopVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CERunLoop/PublicHeader.h>
+
+

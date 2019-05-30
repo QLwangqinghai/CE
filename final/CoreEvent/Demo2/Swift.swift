8d4bc5027e69b30d386b80de33975c12716b508e
vector <dev_vector@qq.com>
2019-05-30 21:15:18 +0800

    no message

---
+++ final/CoreEvent/Demo2/Swift.swift
@@ -0,0 +1,16 @@
+//
+//  Swift.swift
+//  Demo2
+//
+//  Created by vector on 2019/5/30.
+//  Copyright © 2019 com.wangqinghai. All rights reserved.
+//
+
+import Foundation
+import Dispatch
+
+
+DispatchQueue(label: "g", qos: DispatchQoS.utility, attributes: DispatchQueue.Attributes.concurrent, autoreleaseFrequency: DispatchQueue.AutoreleaseFrequency.inherit, target: <#T##DispatchQueue?#>)
+
+
+

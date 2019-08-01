878e76fdfc55ec72aef3a24a8fe327c4f5fa4a42
vector <dev_vector@qq.com>
2019-08-01 21:23:49 +0800

    no message

---
+++ RTC/RTC/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RTC
+//
+//  Created by vector on 2019/8/1.
+//  Copyright © 2019 angfung. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

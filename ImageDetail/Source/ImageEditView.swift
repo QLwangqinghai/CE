1ec3aaf3bc1efb47145e7f7721ef10722094cd75
QLWangqinghai <212032326@qq.com>
2019-09-26 23:46:55 +0800

    no message

--- ImageDetail/Source/ImageEditView.swift
+++ ImageDetail/Source/ImageEditView.swift
@@ -149,7 +149,7 @@ fileprivate protocol _ZoomViewDelegate: class {
     func _zoomViewWillLayoutSubviews()
     func _zoomViewDidLayoutSubviews()
 }
-fileprivate class _ZoomView: UIScrollView {
+open class ZoomView: UIView {
     fileprivate class ZoomContentView: UIView {
         public override init(frame: CGRect) {
             super.init(frame: frame)
@@ -175,99 +175,66 @@ fileprivate class _ZoomView: UIScrollView {
             super.init()
         }
     }
-    
+    fileprivate let scrollView: UIScrollView
+    public let contentView: UIView
+
     public let content: Content
     fileprivate weak var _zoomViewDelegate: _ZoomViewDelegate?
     
+    public private(set) var rotate90CcwCount: UInt = 0
+    
+    private func resetScrollView() {
+        let count = self.rotate90CcwCount
+        if count != 0 {
+            self.scrollView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 2 * Double(count)))
+        } else {
+            self.scrollView.transform = CGAffineTransform()
+        }
+    }
+    
     public override init(frame: CGRect) {
         self.content = Content(containerSize: frame.size)
+        self.scrollView = UIScrollView(frame: CGRect(origin: CGPoint(), size: frame.size))
+        self.contentView = ZoomContentView()
         super.init(frame: frame)
+        
+        self.addSubview(self.scrollView)
+        self.scrollView.addSubview(self.contentView)
     }
     
     required public init?(coder aDecoder: NSCoder) {
         self.content = Content(containerSize: CGSize())
+        self.scrollView = UIScrollView()
+        self.contentView = ZoomContentView()
         super.init(coder: aDecoder)
+        self.addSubview(self.scrollView)
+        self.scrollView.addSubview(self.contentView)
     }
     
     open override func layoutSubviews() {
-        guard let delegate = self._zoomViewDelegate else {
-            super.layoutSubviews()
-            return
-        }
-        delegate._zoomViewDidLayoutSubviews()
+
+        
         super.layoutSubviews()
-        delegate._zoomViewDidLayoutSubviews()
+
     }
     
-}
-
-
-open class ZoomController: NSObject {
-
-    fileprivate class ZoomContentView: UIView {
-        public override init(frame: CGRect) {
-            super.init(frame: frame)
-        }
+    open func rotate90Ccw(animate: Bool) {
+        self .updateRotate90CcwCount(self.rotate90CcwCount + 1, animate: animate)
+    }
+    
+    open func updateRotate90CcwCount(_ count: UInt, animate: Bool) {
+        let value = count % 4
+        if self.rotate90CcwCount != value {
+            self.rotate90CcwCount = value
         
-        required public init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-        }
-    }
-    
-    public class Content: NSObject {
-        public var zoomScale: CGFloat
-        public var size: CGSize
-        public var containerSize: CGSize
-        public var view: UIView {
-            return self._view
-        }
-        fileprivate let _view: ZoomContentView
-        public init(containerSize: CGSize) {
-            self.size = CGSize()
-            self.containerSize = containerSize
-            self._view = ZoomContentView(frame: CGRect(origin: CGPoint(), size: containerSize))
-            self.zoomScale = 1.0
-            super.init()
-        }
-    }
-    
-    public var view: UIView {
-        return self._view
-    }
-    fileprivate let _view: _ZoomView
-
-    
-    public let content: Content
-    //    open override var zoomScale: CGFloat {
-    //        willSet(newValue) {}
-    //        didSet(oldValue) {
-    //
-    //
-    //        }
-    //    }
-    
-    
-    public init(frame: CGRect) {
-        self.content = Content(containerSize: frame.size)
-        self._view = _ZoomView(frame: CGRect(origin: CGPoint(), size: frame.size))
-        super.init()
-        self._view.addSubview(self.content.view)
-        self._view._zoomViewDelegate = self
-    }
-}
-
-extension ZoomController : _ZoomViewDelegate {
-    func _zoomViewWillLayoutSubviews() {
-//        if self.bounds.size != self.content.containerSize {
-//            self.content.containerSize = self.bounds.size
-//        }
-    }
-    func _zoomViewDidLayoutSubviews() {
         
+        
+        }
     }
+    
 }
 
-extension ZoomController : UIScrollViewDelegate {
+extension ZoomView : UIScrollViewDelegate {
     public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         
     }
@@ -288,3 +255,82 @@ extension ZoomController : UIScrollViewDelegate {
         
     }
 }
+
+
+
+//open class ZoomController: NSObject {
+//    fileprivate class ZoomContentView: UIView {
+//        public override init(frame: CGRect) {
+//            super.init(frame: frame)
+//        }
+//
+//        required public init?(coder aDecoder: NSCoder) {
+//            super.init(coder: aDecoder)
+//        }
+//    }
+//
+//    public class Content: NSObject {
+//        public var zoomScale: CGFloat
+//        public var size: CGSize
+//        public var containerSize: CGSize
+//        public var view: UIView {
+//            return self._view
+//        }
+//        fileprivate let _view: ZoomContentView
+//        public init(containerSize: CGSize) {
+//            self.size = CGSize()
+//            self.containerSize = containerSize
+//            self._view = ZoomContentView(frame: CGRect(origin: CGPoint(), size: containerSize))
+//            self.zoomScale = 1.0
+//            super.init()
+//        }
+//    }
+//
+//    public var view: UIView {
+//        return self._view
+//    }
+//    fileprivate let _view: _ZoomView
+//
+//
+//    public let content: Content
+//    public init(frame: CGRect) {
+//        self.content = Content(containerSize: frame.size)
+//        self._view = _ZoomView(frame: CGRect(origin: CGPoint(), size: frame.size))
+//        super.init()
+//        self._view.addSubview(self.content.view)
+//        self._view._zoomViewDelegate = self
+//    }
+//}
+
+//extension ZoomController : _ZoomViewDelegate {
+//    func _zoomViewWillLayoutSubviews() {
+////        if self.bounds.size != self.content.containerSize {
+////            self.content.containerSize = self.bounds.size
+////        }
+//    }
+//    func _zoomViewDidLayoutSubviews() {
+//
+//    }
+//}
+//
+//extension ZoomController : UIScrollViewDelegate {
+//    public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+//
+//    }
+//
+//    public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+//
+//    }
+//
+//    public func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+//        return self.content.view
+//    }
+//
+//    public func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+//
+//    }
+//
+//    public func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+//
+//    }
+//}

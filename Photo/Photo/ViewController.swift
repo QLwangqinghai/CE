a060c1eef28fa01d6f62587b488a43363294fe86
vector <dev_vector@qq.com>
2019-10-14 21:38:41 +0800

    no message

---
+++ Photo/Photo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo
+//
+//  Created by vector on 2019/10/14.
+//  Copyright © 2019 angfung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

60819a87742dd1d7053131009da29b0225761e3e
vector <dev_vector@qq.com>
2019-10-15 21:08:17 +0800

    no message

--- Photo/Photo/AppDelegate.swift
+++ Photo/Photo/AppDelegate.swift
@@ -10,11 +10,15 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let window = UIWindow()
+        self.window = window
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
         return true
     }
 
--- Photo/Photo/ViewController.swift
+++ Photo/Photo/ViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
-
+    var dataSource: AssetDataSource?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+        
+        dataSource = AssetDataSource()
     }
+    
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        PHPhotoLibrary.requestAuthorization { (status) in
+//            if status == .authorized {
+//                AssetGroupDataSource.test()
+//            }
+//        }
+//    }
 
 
 }

c23627829cf1c883f7c7f1e0cf1899cafb14a271
Gogs <gogs@fake.local>
2020-01-20 00:48:35 +0800

    no message

---
+++ ScrollView/DrawingBoard/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  DrawingBoard
+//
+//  Created by vector on 2020/1/18.
+//  Copyright © 2020 angfung. All rights reserved.
+//
+
+import UIKit
+
+
+open class DrawingBoard: UIView {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- ScrollView/Source/Controller.swift
+++ ScrollView/Source/Controller.swift
@@ -8,6 +8,54 @@
 
 import Foundation
 
+
+public class SliceCollection {
+    private class Row {
+        public let slices: [Slice]
+        public init(slices: [Slice]) {
+            self.slices = slices
+        }
+    }
+    private let rows: [Row]
+    
+    private let slicesSize: C2DSize
+    public init?(size: C2DSize, slicesSize: C2DSize) {
+        if slicesSize.width < 0 || slicesSize.height < 0 {
+            return nil
+        }
+        if size.width < 0 || size.height < 0 {
+            return nil
+        }
+        self.slicesSize = slicesSize
+        
+        let numberOfSlicePerRow: Int32
+        if slicesSize.width == 1 {
+            numberOfSlicePerRow = size.width
+        } else {
+            numberOfSlicePerRow = (size.width + slicesSize.width - 1) / slicesSize.width
+        }
+        let rowCount: Int32
+        if slicesSize.height == 1 {
+            rowCount = size.height
+        } else {
+            rowCount = (size.height + slicesSize.height - 1) / slicesSize.height
+        }
+        var rows: [Row] = []
+        for rowIndex in 0 ..< rowCount {
+            let y = slicesSize.height * rowIndex
+            var slices: [Slice] = []
+            slices.reserveCapacity(Int(numberOfSlicePerRow))
+            for sliceIndex in 0 ..< numberOfSlicePerRow {
+                let frame = C2DRectMake(sliceIndex * slicesSize.width, y, slicesSize.width, slicesSize.height)
+                slices.append(Slice(frame: frame))
+            }
+            rows.append(Row(slices: slices))
+        }
+        self.rows = rows
+    }
+}
+
+
 public class DrawingBoardController {
     private class _Row {
         public let slices: [Slice]
